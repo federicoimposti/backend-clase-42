9b3cd527c5704d26fc25ca94041c4a6bf25bbfe5
Federico Imposti <federico.imposti@gmail.com>
2022-12-04 11:42:39 -0300

    add files

---
+++ clase-42/routes/products.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const productsRouter = express.Router();
+
+const { getProducts, saveProducts } = require('../controllers/products.js');
+
+productsRouter.get("/", async (req, res) => {
+  const response = await getProducts();
+  res.status(200).json(response);
+  // res.render('pages/productForm', { products: response });
+});
+
+productsRouter.post("/", (req, res) => {
+  saveProducts(req.body);
+  res.redirect("/");
+});
+
+module.exports = productsRouter;
